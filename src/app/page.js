6e9b7328d96ae2ca69884fcbab5fbf61e4c985ee
Zafar Khan <suparizafar206@gmail.com>
2024-03-25 11:33:54 +0500

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,149 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import image from "@/app/images/image1.png";
+import image1 from "@/app/images/image1.png";
+import image2 from "@/app/images/Rectangle 530.png";
+import image3 from "./images/Rectangle 531.png";
+import image4 from "./images/Rectangle 532.png";
+import Card from "./Card";
+import BorderImage from "./images/Vector 128.svg";
 import Image from "next/image";
+import "./App.css";
 
 export default function Home() {
+  const [showRegions, setShowRegions] = useState(false);
+
+  const toggleRegions = () => {
+    setShowRegions(!showRegions);
+  };
+
+  const handleRegionClick = (region) => {
+    // Assuming your locales are set up in your Next.js project
+    // And you have pages like /en-US, /en-UK, etc.
+    // If not, adjust this logic accordingly
+    window.location.href = `/en-${region.toLowerCase()}`;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="figma">
+        <div className="frame">
+          <div className="header">
+            <p className="font-medium text-xl">NETSOL</p>
+            <div className="drop-down">
+              <button className="btn text-lg " onClick={toggleRegions}>
+                Regions
+              </button>
+              {showRegions && (
+                <div className="dropdown">
+                  <ul>
+                    <li
+                      onClick={() => handleRegionClick("USA")}
+                      // className={
+                      //   originId && originId.includes("usa") && "active"
+                      // }
+                    >
+                      USA
+                      <hr className="border-line" />
+                    </li>
+                    <li
+                      onClick={() => handleRegionClick("UK")}
+                      // className={
+                      //   originId && originId.includes("uk") && "active"
+                      // }
+                    >
+                      UK
+                      <hr className="border-line" />
+                    </li>
+                    <li
+                      onClick={() => handleRegionClick("China")}
+                      // className={
+                      //   originId && originId.includes("china") && "active"
+                      // }
+                    >
+                      China
+                      <hr className="border-line" />
+                    </li>
+                    <li
+                      onClick={() => handleRegionClick("Pakistan")}
+                      // className={
+                      //   originId && originId.includes("pakistan") && "active"
+                      // }
+                    >
+                      Pakistan
+                      <hr className="border-line" />
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          </div>
+          <Image src={BorderImage} />
+          <div className="content mt-5">
+            <button className="text-lg">News and Insight</button>
+            <div className="p3">Latest Insight from Leading Voices</div>
+            <div className="p1">
+              Solving the world’s problems through technology wouldn’t be
+              possible without our most important <br /> invention: the NetSol.
+              Have a look at our talented teams.
+            </div>
+            <div className="p2">
+              View All News
+              <div className="flex justify-center">
+                <hr className="w-[7%] flex justify-center" />
+              </div>
+            </div>
+          </div>
+          <div className="cards">
+            <Card
+              image={image1}
+              btnTitle={"NEWS"}
+              heading={"Leasing Life Conference & Awards"}
+              description={
+                <p>
+                  Solving the world’s problems through technology wouldn’t be
+                  possible without our most important invention.
+                </p>
+              }
             />
-          </a>
+            <Card
+              image={image2}
+              btnTitle={"BLOG"}
+              heading={"Auto Finance Summit 2023"}
+              description={
+                <p>
+                  Solving the world’s problems through technology wouldn’t be
+                  possible without our most important invention.
+                </p>
+              }
+            />
+            <Card
+              image={image3}
+              btnTitle={"NEWS"}
+              heading={"Careers at Netsol"}
+              description={
+                <p>
+                  Solving the world’s problems through technology wouldn’t be
+                  possible without our most important invention.
+                </p>
+              }
+            />
+
+            <Card
+              image={image4}
+              btnTitle={"BLOG"}
+              heading={"Leasing Life Conference & Awards"}
+              description={
+                <p>
+                  Solving the world’s problems through technology wouldn’t be
+                  possible without our most important invention.
+                </p>
+              }
+            />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
